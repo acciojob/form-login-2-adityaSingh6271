c897f6eae77a98298deb86e17c557fc7931a62d5
Aditya Singh <131813027+adityaSingh6271@users.noreply.github.com>
2024-01-10 12:11:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //your JS code here. If required.
 document.getElementById('myForm').addEventListener('submit', function(event) {
-      event.preventDefault(); // Prevents the default form submission
+    event.preventDefault(); // Prevents the default form submission
 
-      // Getting form values
-      const name = document.getElementById('name').value;
-	  const name = document.getElementById('Lastname').value;
-	  const phone = document.getElementById('phone').value;
-      const email = document.getElementById('email').value;
-      
+    // Getting form values
+    const name = document.getElementById('name').value;
+    const Lastname = document.getElementById('Lastname').value;
+    const phone = document.getElementById('phone').value;
+    const email = document.getElementById('email').value;
 
-      // Creating alert message with form info
-      const alertMessage = `Name: ${name}\n Lastname:${Lastname} Phone: ${phone} \nEmail: ${email}`;
+    // Creating alert message with form info
+    const alertMessage = `Name: ${name}\n Last Name:${Lastname}\n Phone: ${phone} \nEmail: ${email}`;
 
-      // Displaying form info in an alert
-      alert(alertMessage);
-    });
+    // Displaying form info in an alert
+    alert(alertMessage);
+});
+
